325d573a31692b1f608040f0b5cb7be985ca36dc
Christopher Williams <cdubone@gmail.com>
2022-09-19 20:53:44 -0700

    Adding some UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,61 +1,16 @@
 <template>
   <v-app>
     <v-navigation-drawer permanent>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <router-view>
+        <nav>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link> |
+          <router-link to="/login">Login</router-link>
+        </nav>
+      </router-view>
     </v-navigation-drawer>
     <p>Navigation Drawer</p>
-    <v-app-bar color="deep-purple accent-4" dense dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Page title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
     <v-main>
@@ -64,14 +19,10 @@
       <v-container fluid>
 
         <!-- If using vue-router -->
-        <router-view></router-view>
+
       </v-container>
     </v-main>
 
-
-    <v-footer app>
-      <p>Footer</p>
-    </v-footer>
   </v-app>
 </template>
 
